3fb6817ee7a1366526daf7990162e80be625a3b8
Mohammad Zaatari <mohamadalzaatari748@gmail.com>
2023-04-05 16:40:02 +0300

    designed the task list

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,19 +1,23 @@
 import { AlignHorizontalCenter } from '@mui/icons-material';
-import { Avatar, Box, LinearProgress, List, ListItem, ListItemAvatar, ListItemText, Container } from '@mui/material';
+import { Avatar, Box, LinearProgress, List, ListItem, ListItemAvatar, ListItemText, Container, Stack, Divider } from '@mui/material';
 
 function TaskList({ tasks }) {
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="sm" sx={{ backgroundColor: '#e3f2fd', border: '1px solid primary.main', borderRadius: 4, boxShadow: 2 }}>
     <h4>In Progress</h4>
     <h6>3 tasks active</h6>
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+    <List sx={{ width: '100%', maxWidth: 560, bgcolor: 'background.paper' }}>
+    <Stack
+    direction="column"
+    divider={<Divider orientation="vertical"/>}
+    spacing={2}>  
       {tasks.map((task, index) => (
         <ListItem key={index}>
           <ListItemAvatar>
             <Avatar>{task.name.charAt(0)}</Avatar>
           </ListItemAvatar>
           <ListItemText primary={task.name} secondary={
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center'}}>
                <p>shared with</p> 
               {task.sharedWith.map((user, index) => (
                 <Avatar key={index} sx={{ ml: 1 }} alt={user} src={`https://i.pravatar.cc/32?u=${user}`} />
@@ -25,6 +29,7 @@ function TaskList({ tasks }) {
           </Box>
         </ListItem>
       ))}
+      </Stack>
     </List>
     </Container>
   );
