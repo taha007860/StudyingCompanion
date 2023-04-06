b86823ce720f662ee61609f49c6c6f8bb0de701e
Adam Harb <blockerduude@gmail.com>
2023-04-06 23:47:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Views from "./Views";
-import {CssBaseline, ThemeProvider} from "@mui/material";
+import { CssBaseline } from "@mui/material";
 
 function App() {
  
     return (
-
             <CssBaseline>
                 <BrowserRouter>
                     <Header />
