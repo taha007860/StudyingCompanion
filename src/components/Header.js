7b9eeef7f9a0f0acfcbc3291b9fd1a1379172a95
Adam Harb <blockerduude@gmail.com>
2023-04-06 23:51:03 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import PlaylistAddCheckIcon from '@mui/icons-material/PlaylistAddCheck';
@@ -83,50 +82,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
-
-=======
-import '../styles/Header.css';
-import {Link} from "react-router-dom";
-import {Button} from "react-bootstrap";
-import {auth} from '../config/firebase';
-import {signOut} from 'firebase/auth';
-
-function Header() {
-    return (
-        <header className='d-flex flex-row'>
-            <div>
-                <Link className='logoStyle' to='/Home'>
-                    <p>Studying Companion </p>
-                </Link>
-            </div>
-            <div className='justify-content-center mx-0'>
-
-                <Link to='/Timer'>
-                    <Button class='btn-lg'>
-                        Timer
-                    </Button>
-                    <Link to='/TaskList'>
-                        <Button class='btn-lg'>
-                            Task List
-                        </Button>
-                    </Link>
-                </Link>
-            </div>
-            <div>
-                <Link to='/auth'>
-                    <Button class='btn-lg' variant='secondary' onClick={ (e) => {
-                        signOut(auth).then(r => console.log("Signed out successfully."));
-                    }}>
-                        Logout ←
-                    </Button>
-                </Link>
-            </div>
-        </header>
-    );
-}
->>>>>>> main
-
