7d1377f409f5cc8478135269afbf838159985d7c
Adam Harb <blockerduude@gmail.com>
2023-04-14 08:31:43 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Views from "./Views";
 import { CssBaseline } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+
 function App() {
   return (
     <CssBaseline>
