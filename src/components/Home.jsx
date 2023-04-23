08bb7f0a9f16b9dd7483f97af1222362cc6b6eb4
joehage <joehage.2003@gmail.com>
2023-04-23 14:39:18 +0300

    Quotes added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,18 +9,38 @@ import {
   Button,
   Avatar,
 } from "@mui/material";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import joe from "../../assets/Joe.jpg";
 import adam from "../../assets/Adam.jpg";
 import zaatar from "../../assets/Zaatar.jpg";
-import animation from "../../assets/animation.gif"
+import animation from "../../assets/animation.gif";
 const Home = () => {
   const navigate = useNavigate();
   const handleLogin = async () => {
     navigate("/auth");
   };
+  const [quote, setQuote] = useState([]);
+  const fetchData = () => {
+    return fetch(
+      "https://api.api-ninjas.com/v1/quotes?limit=1category=knowledge",
+      {
+        headers: {
+          "X-Api-Key": "3p67WCRBW/amdiPOIQWKAg==wPzN4v1uZF34uOzG",
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    fetchData()
+      .then((response) => response.json())
+      .then((data) => setQuote(data))
+      .catch((error) => console.log("Error", error.message));
+  }, []);
+
   return (
     <div
       style={{
@@ -41,20 +61,30 @@ const Home = () => {
           fontFamily: "Open Sans",
           color: "#fff",
         }}
-      > 
+      >
         <Box sx={{ display: "flex", alignItems: "center" }}>
-      <Box sx={{ flexGrow: 1 }}>
-        <Typography variant="h3" sx={{ fontWeight: "bold", color: "black" }}>
-          Studying made easy.
-        </Typography>
-        <Typography variant="h5" sx={{ mt: 3, color: "black" }}>
-          StudyingCompanion helps you organize your study resources, collaborate with peers, and stay on top of your coursework.
-        </Typography>
-      </Box>
-      <Box sx={{ ml: "auto"}}>
-        <img src={animation} alt="Animation" width={"250px"} height={"300px"} style={{ marginLeft: "100px" }} />
-      </Box>
-    </Box>
+          <Box sx={{ flexGrow: 1 }}>
+            <Typography
+              variant="h3"
+              sx={{ fontWeight: "bold", color: "black" }}
+            >
+              Studying made easy.
+            </Typography>
+            <Typography variant="h5" sx={{ mt: 3, color: "black" }}>
+              StudyingCompanion helps you organize your study resources,
+              collaborate with peers, and stay on top of your coursework.
+            </Typography>
+          </Box>
+          <Box sx={{ ml: "auto" }}>
+            <img
+              src={animation}
+              alt="Animation"
+              width={"250px"}
+              height={"300px"}
+              style={{ marginLeft: "100px" }}
+            />
+          </Box>
+        </Box>
         <Button
           variant="contained"
           sx={{ mt: 4, backgroundColor: "#fff", color: "#000" }}
@@ -70,67 +100,67 @@ const Home = () => {
           width: 1 / 2,
         }}
       >
-         <Box
-            sx={{
-              borderRadius: 10,
-              width: 500,
-              fontFamily: "Open Sans",
-              mx: "1rem",
-              marginBottom: "60px",
-              marginLeft: "-20px",
-              marginTop: "-20px"
-            }}
-          >
-            <Accordion sx={{ ml: "20px" }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>Shareable To-Do Lists</Typography>
-              </AccordionSummary>
-              <AccordionDetails sx={{ ml: "20px" }}>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion sx={{ ml: "20px" }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel2a-content"
-                id="panel2a-header"
-              >
-                <Typography>Timers with integrated focus timings</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Using the Pomodoro technique utilized by many around the
-                  world, we aim to improve the average students focus on his
-                  daily tasks on various levels.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-            <Accordion sx={{ ml: "20px" }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>Music to wind down</Typography>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                  eget.
-                </Typography>
-              </AccordionDetails>
-            </Accordion>
-          </Box>
-        <Typography variant="h4" sx={{ fontWeight: "bold", color: "black"}}>
+        <Box
+          sx={{
+            borderRadius: 10,
+            width: 500,
+            fontFamily: "Open Sans",
+            mx: "1rem",
+            marginBottom: "60px",
+            marginLeft: "-20px",
+            marginTop: "-20px",
+          }}
+        >
+          <Accordion sx={{ ml: "20px" }}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Shareable To-Do Lists</Typography>
+            </AccordionSummary>
+            <AccordionDetails sx={{ ml: "20px" }}>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion sx={{ ml: "20px" }}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel2a-content"
+              id="panel2a-header"
+            >
+              <Typography>Timers with integrated focus timings</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Using the Pomodoro technique utilized by many around the world,
+                we aim to improve the average students focus on his daily tasks
+                on various levels.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+          <Accordion sx={{ ml: "20px" }}>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Music to wind down</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        </Box>
+        <Typography variant="h4" sx={{ fontWeight: "bold", color: "black" }}>
           Meet the Team
         </Typography>
         <Grid container spacing={3} sx={{ mt: 3 }}>
@@ -218,9 +248,9 @@ const Home = () => {
           </AccordionSummary>
           <AccordionDetails>
             <Typography variant="body1">
-              StudyingCompanion is a web application designed to help students organize
-              their study resources, collaborate with peers, and stay on top of
-              their coursework.
+              StudyingCompanion is a web application designed to help students
+              organize their study resources, collaborate with peers, and stay
+              on top of their coursework.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -230,8 +260,8 @@ const Home = () => {
           </AccordionSummary>
           <AccordionDetails>
             <Typography variant="body1">
-              To sign up for StudyingCompanion, click on the "Get Started" button and
-              follow the instructions to create an account.
+              To sign up for StudyingCompanion, click on the "Get Started"
+              button and follow the instructions to create an account.
             </Typography>
           </AccordionDetails>
         </Accordion>
@@ -241,8 +271,8 @@ const Home = () => {
           </AccordionSummary>
           <AccordionDetails>
             <Typography variant="body1">
-              If you forget your password, click on the "Forgot Password"
-              button on the login page and follow the instructions to reset your
+              If you forget your password, click on the "Forgot Password" button
+              on the login page and follow the instructions to reset your
               password.
             </Typography>
           </AccordionDetails>
